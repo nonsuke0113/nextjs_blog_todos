36189b081d7e3575fa338651d7e7c2a0a2a7d6fb
nonsuke0113 <asinal.iip36@icloud.com>
2022-03-26 15:46:20 +0900

    create Layout components

---
+++ app/components/Layout.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Layout({ children, title = "Default title"}) {
+  return (
+  <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+    <Head>
+      <title>{title}</title>
+    </Head>
+    <main className="flex flex-1 justify-center items-center text-gray-500 text-sm">
+      {children}
+    </main>
+    <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
+      @nonsuke0113
+    </footer>
+  </div>);
+}
