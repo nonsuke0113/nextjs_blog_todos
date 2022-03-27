4c295190d718bfb007e1f8db50fc37dd6788b504
nonsuke0113 <asinal.iip36@icloud.com>
2022-03-27 12:55:40 +0900

    create main page

---
+++ app/pages/main-page.js
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router';
+import Cookie from "universal-cookie";
+import Layout from '../components/Layout';
+
+const cookie = new Cookie();
+
+export default function MainPage() {
+  
+  const router =useRouter();
+
+  const logout = () => {
+    cookie.remove("access_token");
+    router.push("/");
+  }
+
+  return (
+    <Layout title='main-page'>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="mt-10 cursor-pointer h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+        onClick={logout}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+        />
+      </svg>
+    </Layout>
+  )
+}
