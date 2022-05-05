85ce60ade358d850addb514696dfaf653fe36a9f
nonsuke0113 <asinal.iip36@icloud.com>
2022-05-05 17:38:03 +0900

    fix layout

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children, title = "Default title"}) {
     <Head>
       <title>{title}</title>
     </Head>
-    <main className="flex flex-1 justify-center items-center text-gray-500 text-sm">
+    <main className="flex flex-1 justify-center items-center text-sm flex-col">
       {children}
     </main>
     <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
