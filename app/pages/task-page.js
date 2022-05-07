0de9386101f82275a88eb4d81f2fb72429235935
nonsuke0113 <asinal.iip36@icloud.com>
2022-05-07 13:55:39 +0900

    update task page

--- app/pages/task-page.js
+++ app/pages/task-page.js
@@ -1,9 +1,34 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
+import Task from "../components/Task";
+import { getAllTasksData } from "../lib/tasks";
+import useSWR from "swr";
+import { useEffect } from "react";
 
-export default function TaskPage() {
+const fetcher = (url) => fetch(url).then((res) => res.json());
+const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
+
+export default function TaskPage({ staticFilteredTasks }) {
+
+  const { data: tasks, mutate } = useSWR(apiUrl, fetcher, {
+    fallbackData: staticFilteredTasks,
+  });
+  const filteredTasks = tasks?.sort(
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
+  );
+  
+  useEffect(() => {
+    mutate();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Layout title="Task page">
+      <ul>
+        {filteredTasks &&
+          filteredTasks.map((task) => <Task key={task.id} task={task} />)}
+      </ul>
+
       <Link href="/main-page" passHref>
         <div className="flex cursor-pointer mt-12">      
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
@@ -14,4 +39,12 @@ export default function TaskPage() {
       </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const staticFilteredTasks = await getAllTasksData();
+  return {
+    props: { staticFilteredTasks },
+    revalidate: 3,
+  }
 }
